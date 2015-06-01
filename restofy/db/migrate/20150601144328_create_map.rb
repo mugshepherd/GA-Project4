c73aed79147d2f06de780a50b0db529b2cee6776
Jared Yamauchi <jared.yamauchi@gmail.com>
2015-06-01 10:53:51 -0400

    Setting up the User devise

---
+++ restofy/db/migrate/20150601144328_create_map.rb
@@ -0,0 +1,6 @@
+class CreateMap < ActiveRecord::Migration
+  def change
+    create_table :maps do |t|
+    end
+  end
+end
---
+++ restofy/db/migrate/20150601145214_create_profile.rb
@@ -0,0 +1,6 @@
+class CreateProfile < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+    end
+  end
+end
