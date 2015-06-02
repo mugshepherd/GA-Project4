b2c2f5a767c6e30981571c8433b558ec2ab90be6
Jared Yamauchi <jared.yamauchi@gmail.com>
2015-06-02 11:25:32 -0400

    update the db models

--- restofy/db/migrate/20150601151751_create_comments.rb
+++ restofy/db/migrate/20150601151751_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+    	t.belongs_to :profile
+
+    	t.text :content
+    	t.timestamps null: false 
     end
   end
 end
--- restofy/db/migrate/20150601151759_create_profiles.rb
+++ restofy/db/migrate/20150601151759_create_profiles.rb
@@ -1,6 +1,14 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
+    	t.belongs_to :user
+
+    	t.text :name
+    	t.text :photo_url
+    	t.text :hometown
+    	t.text :interests
+    	t.text :about_me
+    	t.timestamps null: false
     end
   end
 end
