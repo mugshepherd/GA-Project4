42ff65d9bb226a9fb18eefadfd45125815b3c505
Toby <toby.schaeffer@gmail.com>
2015-06-04 16:40:36 -0400

    pre JY mapbox tutorial.  changing all css etc.

--- restofy/app/assets/javascripts/models/map.js
+++ restofy/app/assets/javascripts/models/map.js
@@ -2,23 +2,50 @@
 
 
 
-// $(document).ready(function() {
+$(document).ready(function() {
+	$(".ul-content").bind("click", function( event ) {
+		alert ("the mouse cursor is at (" + 
+				event.pageX + ", " + event.pageY +
+				")" );
+	})
+});
+
+
+
+
 //   L.mapbox.accessToken = 'pk.eyJ1IjoidHNjaGFlZmZlcjc1MDQiLCJhIjoibGtEVEpDTSJ9.Ii90udJG3hfV3ftGYafKLQ';
 //   var map = L.mapbox.map('map', 'tschaeffer7504.mbaooln6').setView([38.899, -77.015], 12);
 //   var myLayer = L.mapbox.featureLayer().addTo(map);
  
+	// $('.restaurant-ul').on('click', function(e) {
+	// 	var current, currentlyClickedName;
+	// 	current = $(this);
 
-  // myLayer.on('layeradd', function(e) {
-  //   var marker, popupContent, properties;
-  //   marker = e.layer;
-  //   properties = marker.feature.properties;
-  //   popupContent = '<div class="popup"><h3><a href="/markers/' + properties.id + '" target="marker">' + properties.name + '</a></h3><p class="popup-num">Marker No. ' + properties.number + '</p><p>' + properties.description + '</p></div>';
-  //   return marker.bindPopup(popupContent, {
-  //     closeButton: false,
-  //     minWidth: 300
-  //   });
-  // });
 
+
+// 	  var current, currentlyClickedName;
+// 	  current = $(this);
+// 	  currentlyClickedName = current.find('.restaurant-name-li').text();
+// 	  return map.featureLayer.eachLayer(function(marker) {
+// 	    var id;
+// 	    if (marker.feature.properties.title === currentlyClickedName) {
+// 	      id = layeradd._leaflet_id;
+// 	      return map._layers[id].openPopup();
+// 	    }
+// 	  });
+// 	});
+
+//   myLayer.on('layeradd', function(e) {
+//     var marker, popupContent, properties;
+//     marker = e.layer;
+//     properties = marker.feature.properties;
+//     popupContent = '<div class="popup"><h3><a href="/restaurants/' + properties.id + '" target="marker">' + properties.title + '</a></h3><p class="popup-num">Marker No. ' + properties.title + '</p><p>' + properties.description + '</p></div>';
+
+//     return marker.bindPopup(popupContent, {
+//       closeButton: false,
+//       minWidth: 300
+//     });
+//   });
 
 //   $.ajax({
 //     dataType: 'text',
@@ -31,15 +58,3 @@
 //   });
 // });
 
-// 	$('article li').click(function(e) {
-// 	  var current, currentlyClickedName;
-// 	  current = $(this);
-// 	  currentlyClickedName = current.find('h2').text();
-// 	  return map.featureLayer.eachLayer(function(marker) {
-// 	    var id;
-// 	    if (marker.feature.properties.name === currentlyClickedName) {
-// 	      id = layer._leaflet_id;
-// 	      return map._layers[id].openPopup();
-// 	    }
-// 	  });
-// 	});
--- restofy/app/models/inspection.rb
+++ restofy/app/models/inspection.rb
@@ -1,5 +1,6 @@
 class Inspection < ActiveRecord::Base
   belongs_to :restaurant
+  has_many :violations
   has_many :potential_violations, through: :violations
   # validates :restaurant_id, presence: true
 end
